874e78c8e40f8dd29aab423e6b87ab8bb9dd55d9
Mario <marionikolov87@gmail.com>
2021-04-08 08:05:21 +0300

    add form; add funcs and reducer;

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -17,23 +17,23 @@ const ContactState = props => {
 		contacts: [
 			{
 				id: 1,
-				name: 'Name 1',
-				email: 'email@name1.com',
-				phone: '12345',
+				name: 'Sub Zero',
+				email: 'zero@gmail.com',
+				phone: '111-111-111',
 				type: 'personal',
 			},
 			{
 				id: 2,
-				name: 'Name 2',
-				email: 'email@name2.com',
-				phone: '12345',
+				name: 'Liu Kang',
+				email: 'lkang@gmail.com',
+				phone: '222-222-222',
 				type: 'personal',
 			},
 			{
 				id: 3,
-				name: 'Name 3',
-				email: 'email@name3.com',
-				phone: '12345',
+				name: 'Sonya Blade',
+				email: 'sonya@gmail.com',
+				phone: '333-333-333',
 				type: 'professional',
 			},
 		],
@@ -42,6 +42,10 @@ const ContactState = props => {
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	// Add Contact
+	const addContact = contact => {
+		contact.id = uuid();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 
 	// Delete Contact
 
@@ -56,7 +60,7 @@ const ContactState = props => {
 	// Clear Filter
 
 	return (
-		<ContactContext.Provider value={{ contacts: state.contacts }}>
+		<ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
 			{props.children}
 		</ContactContext.Provider>
 	);

a6913701c888e1a5314c2678272b44e7515effb5
Mario <marionikolov87@gmail.com>
2021-04-17 13:56:15 +0300

    deploy settings;

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,15 @@ const url = process.env.MAIN_URL || '/api';
 app.use(`${url}/users`, require('./routes/users'));
 app.use(`${url}/contacts`, require('./routes/contacts'));
 app.use(`${url}/auth`, require('./routes/auth'));
+
+// Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 

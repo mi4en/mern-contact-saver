7230fdf33f01fe78cae976af5e943e5d5420452f
Mario <marionikolov87@gmail.com>
2021-04-17 16:30:39 +0300

    fix for deployment;

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import AlertContext from '../../context/alert/alertContext';
+import AlertContext from '../../context/alert/alertContext.js';
 
 const Alerts = () => {
 	const alertContext = useContext(AlertContext);
